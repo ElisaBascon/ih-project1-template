5e802b00aba4b4b46b83572c5821962354b29bf2
elisa <bascongarcia@gmail.com>
2022-05-07 12:55:09 +0200

    canvas fijado y player hec ho

---
+++ js/player.js
@@ -0,0 +1,11 @@
+class Pana {
+    constructor(x, y, width, height) {
+        this.x = 200;
+        this.y = 700;
+        this.width = 20;
+        this.height = 20;  
+        this.lifePoints = 0;
+    }
+
+}
+
