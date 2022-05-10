68b8933faa7d8fd0d6e03ad2fafcac0096e4303a
Ale Bausa <alebausa@MacBook-Pro-de-Ale.local>
2022-05-10 21:10:37 +0200

    Updates generate enemy function

--- js/enemies.js
+++ js/enemies.js
@@ -1,9 +1,9 @@
 const enemies = [
   new Enemy(50, 200, 110, 100, 'good'),
-  new Enemy(50, 200, 110, 100, 'bad'),
-  new Enemy(50, 200, 110, 100, 'good'),
   new Enemy(250, 200, 110, 100, 'good'),
+  new Enemy(50, 200, 110, 100, 'good'),
   new Enemy(250, 200, 110, 100, 'bad'),
+  new Enemy(50, 200, 110, 100, 'bad'),
   new Enemy(250, 200, 110, 100, 'good'),
 ]
     
--- js/game.js
+++ js/game.js
@@ -4,31 +4,51 @@ class Game{
     this.pana = new Player(150, 590, 110, 110);
     this.enemies = enemies;
     this.enemyInterval = undefined;
+    this.paintingEnemy = undefined;
   }
 
   _drawPana() {
     this.ctx.drawImage(pana, this.pana.x, this.pana.y, this.pana.width, this.pana.height);
   }
 
+  _assignCurrentEnemy() {
+    let currentEnemy = 0;
+    this.enemyInterval = setInterval(() => { 
+      this.paintingEnemy = enemies[currentEnemy];
+      currentEnemy++;
+    }, 2000);
+  }
+
   _drawEnemies() {
-    let counter = 0; 
-    this.enemyInterval = setInterval(() => {
-       for (let i = 0; i < enemies.length; i++) {	
-      counter = counter + i;
+    if (this.paintingEnemy) {
+      if (this.paintingEnemy.role == 'bad') {
+            this.ctx.drawImage(badEnemie, this.paintingEnemy.x, this.paintingEnemy.y, this.paintingEnemy.width, this.paintingEnemy.height);
+          } else if (enemies[0].role == 'good') {
+            this.ctx.drawImage(goodEnemie, this.paintingEnemy.x, this.paintingEnemy.y, this.paintingEnemy.width, this.paintingEnemy.height);
+          }
     }
-  }, 2000);
-  console.log(counter);
-} 
+  } 
+
+  checkRight() {
+    // Si lo que hay this.paintingEnemy: posición? Derecha?
+    // Si this.paintingEnemy.role == 'good'? => puntos // 'bad' => resto puntos
+  }
+
+  checkLeft() {
+    // Si lo que hay this.paintingEnemy.x < 200: posición? Izquierda?
+    // Si this.paintingEnemy.role == 'good'? => puntos // 'bad' => resto puntos
+  }
+
 
   _assignControls() {
     // Controles del teclado
     document.addEventListener('keydown', (event) => {
       switch (event.code) {
         case 'ArrowLeft':
-          this.meatball.moveLeft();
+          this.checkLeft();
           break;
         case 'ArrowRight':
-          this.meatball.moveRight();
+          this.checkRight();
           break;
         default:
           break;
@@ -49,6 +69,7 @@ class Game{
 
   start() {
     this._assignControls();
+    this._assignCurrentEnemy();
     this._update();
   }
 }
